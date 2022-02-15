160c7aec27c051c73f0047c99e48c1b679839c62
sezin <rlatpwls15@naver.com>
2022-02-15 17:41:07 +0900

    부분집합

--- Backjoon/Algorithm/src/B_2961.java
+++ Backjoon/Algorithm/src/B_2961.java
@@ -23,10 +23,32 @@ public class B_2961 {
 			bitter[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		dfs(0);
+//		dfs(0);
+		binary(0);
 		System.out.println(result);
 	}
-	
+	static void binary(int cnt)
+	{
+		//바이너리 카운팅을 이용한 부분집합
+		//N=10이면 비트를 이용해서 각 요소의 자리수에 해당하는 경우의 수를 만들 수 있음
+		//0000000000~1111111111
+		//부분집합의 개수 = 2^N개
+		for (int i = 0; i <(1<<N); i++) {
+			int t_sour = 1;
+			int t_bitter = 0;
+			for(int j = 0; j<N;j++)
+			{
+				if((i&(1<<j)) !=0) 
+				{
+					t_sour *= sour[j];
+					t_bitter += bitter[j];
+				}
+			}
+			//공집합인지 아닌지 확인
+			if(!(t_sour==1&&t_bitter==0) && result > Math.abs(t_sour-t_bitter))
+				result = Math.abs(t_sour-t_bitter);
+		}
+	}
 	static void dfs(int cnt)
 	{
 		if(cnt==N)
