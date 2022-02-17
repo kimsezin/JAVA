1adba7bd7e9f4c1a4832546dc2ee838b7e897166
sezin <rlatpwls15@naver.com>
2022-02-17 13:38:36 +0900

    BackTracking + Greedy

---
+++ Backjoon/Algorithm/src/B_3109.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_3109 {
+	static int R,C;
+	static char map[][];
+	
+	static int dx[] = {-1,0,1}; //위, 옆, 아래
+	static int dy[] = {1,1,1}; // 오른쪽 한칸
+	
+	static int result=0; // 순서 카운팅
+	static boolean check; // 한번 도달했는지 확인용
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		
+		for(int i=0;i<R;i++)
+		{
+			String temp = br.readLine();
+			for(int j=0;j<C;j++)
+				map[i][j] = temp.charAt(j);
+		}
+		for(int i=0;i<R;i++)
+		{
+			check=false; // i행에서 출발한 파이프가 가장 오른쪽 행에 닿았을 때 true
+			recur(i,0);
+		}
+		System.out.println(result);
+	}
+
+	static void recur(int row, int col)
+	{
+		if(col==C-1)
+		{
+			check=true;
+			result++;
+			return;
+		}	
+		
+		
+		for(int i=0;i<3;i++)
+		{
+			int nx = row+dx[i]; //위, 옆, 아래
+			int ny = col+dy[i]; // 오른쪽 1칸
+			
+			if(nx >=0 && nx<R && ny < C)
+			{
+				if(map[nx][ny]=='.') // 도착한 칸이 .이고 이 전 행에서 들리지 않은 곳일때
+				{
+					map[nx][ny]='x';
+					recur(nx,ny);
+					if(check==true)
+						return;
+				}
+			}
+		}
+	
+	}
+}
