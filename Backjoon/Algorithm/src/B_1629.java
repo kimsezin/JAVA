f6ff02031b48ba72c5c6f0cfb68a0242adf47448
sezin <rlatpwls15@naver.com>
2022-02-18 21:35:30 +0900

    Divide&Conquer

---
+++ Backjoon/Algorithm/src/B_1629.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_1629 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		long result = exp((long)A,B,C);
+		System.out.println(result%C);
+	}
+
+	static long exp(long A, int size, int c)
+	{
+		if(size==1)
+			return A;
+		long result = exp(A,size/2,c);
+		if(size%2==0)
+			return result*result%c;
+		else
+			return (result*result%c)*A%c;
+		
+	}
+}
