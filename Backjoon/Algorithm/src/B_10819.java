51f4cd92362265febef5fad4c64566663a0bbb80
sezin <rlatpwls15@naver.com>
2022-02-18 21:15:46 +0900

    BackTracking

---
+++ Backjoon/Algorithm/src/B_10819.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_10819 {
+	static int arr[];
+	static int result=0;
+	static int totalcnt=0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		arr = new int[N];
+		boolean[] visited = new boolean[N];
+		for(int i=0;i<N;i++)
+			arr[i]=Integer.parseInt(st.nextToken());
+		
+		dfs(0, N, new int[N],visited);
+		System.out.println(result);
+	}
+	
+	static void dfs(int cnt, int N, int[] temp, boolean[] visited)
+	{
+		if(cnt==N)
+		{
+			int sum=0;
+			for(int i=0;i<N-1;i++)
+				sum+=Math.abs(temp[i]-temp[i+1]);
+			if(sum>result)
+				result=sum;
+			return;
+		}
+		for(int i=0;i<N;i++)
+		{
+			if(visited[i]==false)
+			{
+				visited[i]=true;
+				temp[cnt]=arr[i];
+				dfs(cnt+1,N,temp,visited);
+				temp[cnt]=0;
+				visited[i]=false;
+			}
+		}
+	}
+	
+	
+}
