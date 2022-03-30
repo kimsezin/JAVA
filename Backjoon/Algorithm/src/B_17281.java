ed6afc283552f653a39088db41aa0545d98f1c04
sezin <rlatpwls15@naver.com>
2022-03-30 10:05:45 +0900

    Permutation

--- Backjoon/Algorithm/src/B_17281.java
+++ Backjoon/Algorithm/src/B_17281.java
@@ -8,101 +8,159 @@ import java.util.StringTokenizer;
 
 public class B_17281 {
 	static int inning, order[], hitter[][];
-	static boolean visited[];
+	static boolean player[];
 	static int result=0;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		//총 진행하는 이닝 수
 		inning = Integer.parseInt(br.readLine());
-		visited = new boolean[9];
-		order = new int[9]; //타순
-		hitter=new int[inning][9]; //이닝 당 타자들 기록
+		//타자들 이닝 별 타구기록
+		hitter = new int[inning][9];
+		//타순
+		order = new int[9];
+		//타자들 쳤는지 확인하기위함
+		player = new boolean[9];
 		
 		for(int i=0;i<inning;i++)
 		{
 			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			for(int j=0;j<9;j++)
-				hitter[i][j]=Integer.parseInt(st.nextToken());
+				hitter[i][j] = Integer.parseInt(st.nextToken());
 		}
 		
-		//1번 선수가 4번타자 고정이므로 
-		
-		visited[0]=true;
 		order[3]=0;
-		permutation(1);
-		
+		player[0]=true;
+		permutation(0);
 		System.out.println(result);
-		System.out.println(Count);
+	}
+	static void permutation(int cnt)
+	{
+		if(cnt==3)
+		{
+			permutation(cnt+1);
+			return;
+		}
+		if(cnt==9)
+		{
+			play();
+			return;
+		}
+		else
+		{
+			for(int i=0;i<9;i++)
+			{
+				if(player[i]==false)
+				{
+					player[i]=true;
+					order[cnt]=i;
+					permutation(cnt+1);
+					player[i]=false;
+				}
+			}
+		}
 	}
 	
 	static void play()
 	{
-		int out=0;
+		//이번 경기에서 나오는 점수
 		int score=0;
 		int idx=0;
-		int base[] = new int[3];
 		for(int i=0;i<inning;i++)
 		{
-			out=0;
-			base = new int[3];
-			while(out!=3)
+			//각 베이스 상태저장용
+			int base[] = new int[3];
+			int outCnt=0;
+			while(true)
 			{
+				//순서대로 타자 나와서 치기
 				idx%=9;
 				int hit = hitter[i][order[idx]];
-				
-				switch(hit) {
-				case 0:
-					out++;
-					break;
-				case 4:
-					score+=base[0]+base[1]+base[2]+1;
-					base[0]=0;base[1]=0;base[2]=0;
-					break;
-				case 3:
-					score+=base[2]+base[1]+base[0];
-					base[2]=1;base[1]=0;base[0]=0;
-					break;
-				case 2: 
-					score+=base[2]+base[1];
-					base[2]=base[0];base[1]=1;base[0]=0;
-					break;
-				case 1: 
-					score+=base[2];
-					base[2]=base[1];base[1]=base[0];base[0]=1;
-					break;
+				switch(hit)
+				{
+					//아웃
+					case 0: 
+						outCnt++;
+						break;
+					//안타
+					case 1:
+						if(base[2]==1)
+						{
+							score++;
+							base[2]=0;
+						}
+						if(base[1]==1)
+						{
+							base[2]=1;
+							base[1]=0;
+						}
+						if(base[0]==1)
+							base[1]=1;
+						
+						base[0]=1;
+						break;
+					//2루타
+					case 2:
+						if(base[2]==1)
+						{
+							score++;
+							base[2]=0;
+						}
+						if(base[1]==1)
+						{
+							score++;
+							base[1]=0;
+						}
+						if(base[0]==1)
+						{
+							base[2]=1;
+							base[0]=0;
+						}
+						base[1]=1;
+						break;
+					//3루타
+					case 3:
+						if(base[2]==1)
+							score++;
+						if(base[1]==1)
+						{
+							score++;
+							base[1]=0;
+						}
+						if(base[0]==1)
+						{
+							score++;
+							base[0]=0;
+						}
+						base[2]=1;
+						break;
+					//홈런
+					case 4:
+						if(base[2]==1)
+						{
+							score++;
+							base[2]=0;
+						}
+						if(base[1]==1)
+						{
+							score++;
+							base[1]=0;
+						}
+						if(base[0]==1)
+						{
+							score++;
+							base[0]=0;
+						}
+						score++;
+						break;
 				}
 				idx++;
+				if(outCnt==3)
+					break;
+				
 			}
 		}
 		if(result < score)
 			result=score;
 	}
-	static int Count=0;
-	static void permutation(int cnt)
-	{
-		if(cnt==3)
-			permutation(cnt+1);
-		
-		if(cnt==9)
-		{
-//			for(int i=0;i<9;i++)
-//				System.out.print(order[i]);
-//			System.out.println();
-			Count++;
-			play();
-			return;
-		}
-		for(int i=1;i<9;i++)
-		{		
-			if(visited[i]==false)
-			{
-				visited[i] = true;
-				order[cnt]=i;
-				permutation(cnt+1);
-				visited[i]=false;
-			}
-		}
-	}
-
-	
 }
