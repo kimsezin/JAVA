1d54c97da610389cf28a19fdf8ceabb02a2de515
sezin <rlatpwls15@naver.com>
2022-03-30 10:05:02 +0900

    Greedy

---
+++ Backjoon/Algorithm/src/B_14501.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class B_14501 {
+	public static class consulting implements Comparable<consulting>
+	{
+		int day,pay;
+
+		public consulting(int day, int pay) {
+			super();
+			this.day = day;
+			this.pay = pay;
+		}
+		@Override
+		public int compareTo(consulting o) {
+			// TODO Auto-generated method stub
+			return -(this.pay-o.pay);
+		}
+		
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		ArrayList<ArrayList<consulting>> result = new ArrayList<>();
+		for(int i=0;i<=N;i++)
+		{
+			result.add(new ArrayList<consulting>());
+		}
+		
+		for(int i=0;i<N;i++)
+		{
+			st = new StringTokenizer(br.readLine()," ");
+			int after = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			if(i+after <= N)
+			{
+				result.get(i+after).add(new consulting(after,value));	
+			}
+		}
+		int sum=0;
+		int start = 0;
+		for(int i=0;i<=N;i++)
+		{
+			if(result.get(i).size()!=0)
+			{
+				Collections.sort(result.get(i));
+				for(int j=0;j<result.get(i).size();j++)
+				{
+					if(start+result.get(i).get(j).day <= i)
+					{
+						sum+=result.get(i).get(j).pay;
+						start = i;
+						break;
+					}
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ Backjoon/Algorithm/src/B_2304.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//해결필요
+public class B_2304 {
+
+	static class top
+	{
+		int width, height;
+
+		public top(int width, int height) {
+			super();
+			this.width = width;
+			this.height = height;
+		}
+	
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;		
+		int input[] = new int[1001];
+		int start = 1001;
+		int end = 0;
+		int sum=0;
+		for(int i=0;i<N;i++)
+		{
+			st = new StringTokenizer(br.readLine()," ");
+			int width = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			if(start > width)
+				start = width;
+			if(end < width)
+				end = width;
+			input[width] = height;
+		}
+		int checkLeft=start;
+		int checkRight=end;
+		while(start<=end)
+		{
+			if(input[++start] >= input[checkLeft])
+			{
+				sum+= (start-checkLeft)*input[checkLeft];
+				checkLeft = start;
+			}
+			if(input[--end] > input[checkRight])
+			{
+				sum+= (checkRight - end) *input[checkRight];
+				checkRight=end;
+			}
+		}
+		sum+=input[checkLeft];
+		System.out.println(sum);
+	}
+
+}
