db3b50b849b3d5cf5cd55574f61b5eed938891f3
sezin <rlatpwls15@naver.com>
2022-04-03 23:33:40 +0900

    BFS/구현

--- Backjoon/Algorithm/src/B_16234.java
+++ Backjoon/Algorithm/src/B_16234.java
@@ -45,7 +45,7 @@ public class B_16234 {
 			}
 		}
 		
-		while(solve())
+		while(solve()) // bfs를 돌릴 수 있는 환경인지 확인해 돌릴 수 있으면 무한 반복 => 인구이동을 할 수 있다는 뜻
 		{
 			
 		}
@@ -55,9 +55,9 @@ public class B_16234 {
 	}
 	
 	private static boolean solve() {
-		result++;
+		result++; // 인구이동 횟수 증가
 		boolean check = false;
-		visited = new boolean[N][N];
+		visited = new boolean[N][N]; 
 		for(int i=0;i<N;i++)
 			for(int j=0;j<N;j++)
 			{
@@ -68,10 +68,10 @@ public class B_16234 {
 					
 					if(nx >= 0 && ny >= 0 && nx < N && ny < N && visited[nx][ny]==false)
 					{
-						int gap = Math.abs(map[i][j]- map[nx][ny]);
+						int gap = Math.abs(map[i][j]- map[nx][ny]); // 두 나라사이의 인구차이가 L이상 R이하일때
 						if(L <= gap && gap <= R)
 						{
-							q.offer(new location(i,j));
+							q.offer(new location(i,j)); //bfs돌리기 위해 q에 넣기
 							bfs();
 							check= true;
 						}
@@ -87,16 +87,16 @@ public class B_16234 {
 	public static void bfs()
 	{
 		
-		boolean checked[][] = new boolean[N][N];
+		boolean checked[][] = new boolean[N][N]; //현재 찾은 나라에서 근접한 bfs만 돌리므로 따로checked라는 visited성질을 갖는 배열 생성
 		int cnt = 0;
 		int sum = 0;
 		while(!q.isEmpty())
 		{
 			location temp = q.poll();
-			visited[temp.x][temp.y]=true;
-			checked[temp.x][temp.y]=true;
-			cnt++;
-			sum+=copy_map[temp.x][temp.y];
+			visited[temp.x][temp.y]=true; // 해당 bfs가 끝나고 같은 지점을 다시 확인하지 않기위해
+			checked[temp.x][temp.y]=true; // 현재  bfs를 위한 checked배열
+			cnt++; // 인구 합칠 나라 카운트
+			sum+=copy_map[temp.x][temp.y]; // 인구합
 			for(int i=0;i<4;i++)
 			{
 				int nx = temp.x + dx[i];
@@ -105,7 +105,7 @@ public class B_16234 {
 				if(nx >= 0 && ny >= 0 && nx < N && ny < N && visited[nx][ny]==false)
 				{
 					
-					int gap = Math.abs(copy_map[temp.x][temp.y]- copy_map[nx][ny]);
+					int gap = Math.abs(copy_map[temp.x][temp.y]- copy_map[nx][ny]); // 기존의 map이 영향을 받지 않도록 copy_map에 bfs적용
 					if(L <= gap && gap <= R)
 					{
 						visited[nx][ny]=true;
@@ -121,8 +121,8 @@ public class B_16234 {
 		{
 			for(int j=0;j<N;j++)
 			{
-				if(checked[i][j]==true)
-					copy_map[i][j] = sum/cnt;
+				if(checked[i][j]==true) //check된 부분은 들린 부분으로 인구를 합쳐야함
+					copy_map[i][j] = sum/cnt; // 인구합/합칠나라로 값 재할당
 			}
 		}
 	}
